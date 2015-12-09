26681fb637599c7c45cc4035f4aae92adf886117
Bibby <bibbynet@gmail.com>
2015-12-09 19:39:33 +0800

    fix compiling errors

--- test/MyMVC6Template.Core.Test/Class1.cs
+++ test/MyMVC6Template.Core.Test/Class1.cs
@@ -14,7 +14,7 @@ namespace MyFirstDnxUnitTests
         [Fact]
         public void FailingTest()
         {
-            Assert.Equal(5, Add(2, 2));
+            Assert.Equal(5, Add(2, 3));
         }
 
         int Add(int x, int y)
