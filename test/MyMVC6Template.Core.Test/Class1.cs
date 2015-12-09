239094bff022361c53bd2cc0cc43c223b92e4ad9
Bibby <bibbynet@gmail.com>
2015-12-09 17:48:30 +0800

    add test core project

---
+++ test/MyMVC6Template.Core.Test/Class1.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+
+namespace MyFirstDnxUnitTests
+{
+    //https://xunit.github.io/docs/getting-started-dnx.html
+    public class Class1
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(5, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
